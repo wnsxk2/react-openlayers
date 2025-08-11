f0cc90ec40dac48f54f51ecef9b633ed77b4d38e
wnsxk2 <wnsxk206@gmail.com>
2025-08-11 19:46:01 +0900

    test(auth): 로그인, 회원가입 테스트 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,43 @@ import { css } from '@emotion/react';
 import axios from 'axios';
 
 export default function HomePage() {
-  const handleButtonClick = async () => {
-    const { data } = await axios.post('/api/v1/auth/id', { id: 'nasssdf' });
+  const handleSigninButtonClick = async () => {
+    const { data } = await axios.post('/api/v1/auth/signup', {
+      id: 'gb1234',
+      password: '12341234',
+      email: 'test@naver.com',
+      username: '홍길동',
+      tel: '010-1234-5678',
+    });
+    console.log('data : ', data);
+  };
+  const handleLoginButtonClick = async () => {
+    const { data } = await axios.post('/api/v1/auth/login', {
+      id: 'gb1234',
+      password: '12341234',
+    });
+    console.log('data : ', data);
+  };
+  const handleIdCheckButtonClick = async () => {
+    const { data } = await axios.post('/api/v1/auth/id', {
+      id: 'gb1234',
+    });
     console.log('data : ', data);
   };
   return (
     <div>
       <h1 css={test}>🏠 Home Page</h1>
-      <button css={btn} onClick={handleButtonClick}>
-        클릭하세요
-      </button>
+      <div css={btnSection}>
+        <button css={btn} onClick={handleLoginButtonClick}>
+          로그인
+        </button>
+        <button css={btn} onClick={handleSigninButtonClick}>
+          회원가입
+        </button>
+        <button css={btn} onClick={handleIdCheckButtonClick}>
+          아이디 중복 확인
+        </button>
+      </div>
     </div>
   );
 }
@@ -26,4 +53,10 @@ const btn = css`
   &:hover {
     background-color: red;
   }
+`;
+
+const btnSection = css`
+  display: flex;
+  flex-direction: column;
+  gap: 12;
 `;
