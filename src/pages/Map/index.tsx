3be40f4435d62399388c107d47ebe2ef11427de1
wnsxk2 <wnsxk206@gmail.com>
2025-08-11 11:22:33 +0900

    feat(map): menu icon button 구현

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,3 +1,35 @@
+import { colors } from '@/shared/styles';
+import { css } from '@emotion/react';
+import { IoMenuOutline } from 'react-icons/io5';
+
 export default function MapPage() {
-  return <div>index</div>;
+  return (
+    <div>
+      <button
+        css={menuBtn}
+        onClick={() => {
+          console.log('Clicked menu');
+        }}
+      >
+        <IoMenuOutline />
+      </button>
+      <div>index</div>
+    </div>
+  );
 }
+
+const menuBtn = css`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  z-index: 100;
+  background-color: ${colors.white};
+  border: 1px solid ${colors.borderLight};
+  border-radius: 50%;
+  box-shadow: 2px 0 8px ${colors.shadowLight};
+`;
