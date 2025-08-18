c64457b8f0b906f8b4e38101d3acecca2a1cdb48
wnsxk2 <wnsxk206@gmail.com>
2025-08-18 15:59:22 +0900

    test(map): polygon 위치 정보 조회 mock api 구현

--- src/mocks/map/constant.ts
+++ src/mocks/map/constant.ts
@@ -1,6 +1,6 @@
-import type { GeoJson, PointInfo } from '@/mocks/map/type';
+import type { Geometry, PointInfo } from '@/mocks/map/type';
 
-export const points: GeoJson<PointInfo>[] = [
+export const points: Geometry<PointInfo>[] = [
   {
     geom: 'POINT(14368827.101334965 4172067.81586528)',
     properties: {
@@ -65,6 +65,107 @@ export const points: GeoJson<PointInfo>[] = [
       importance: '높음',
       latitude: 37.5133,
       longitude: 127.1058,
+    },
+  },
+];
+
+export const polygonList = [
+  {
+    name: '종로구',
+    description: '서울특별시의 중심부에 위치한 역사와 문화의 중심지입니다.',
+    category: '행정구역',
+    area: 23.91,
+    center: [126.977969, 37.57295],
+    location: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [127.0276, 37.4979],
+          [127.06, 37.4979],
+          [127.06, 37.52],
+          [127.0276, 37.52],
+          [127.0276, 37.4979],
+        ],
+      ],
+    },
+  },
+  {
+    name: '강남구',
+    description: '대한민국의 경제, 비즈니스, 패션의 중심지 중 하나입니다.',
+    category: '행정구역',
+    area: 39.55,
+    center: [127.047502, 37.517331],
+    location: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [127.02, 37.49],
+          [127.06, 37.49],
+          [127.06, 37.52],
+          [127.02, 37.52],
+          [127.02, 37.49],
+        ],
+      ],
+    },
+  },
+  {
+    name: '마포구',
+    description:
+      '홍대 거리로 대표되는 젊음과 예술, 그리고 다양한 문화가 공존하는 지역입니다.',
+    category: '행정구역',
+    area: 23.85,
+    center: [126.908421, 37.563914],
+    location: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [126.89, 37.53],
+          [126.95, 37.53],
+          [126.95, 37.57],
+          [126.89, 37.57],
+          [126.89, 37.53],
+        ],
+      ],
+    },
+  },
+  {
+    name: '송파구',
+    description:
+      '서울의 남동쪽에 위치하며, 올림픽공원과 롯데월드타워로 유명합니다.',
+    category: '행정구역',
+    area: 33.87,
+    center: [127.105862, 37.514544],
+    location: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [127.08, 37.5],
+          [127.12, 37.5],
+          [127.12, 37.53],
+          [127.08, 37.53],
+          [127.08, 37.5],
+        ],
+      ],
+    },
+  },
+  {
+    name: '영등포구',
+    description:
+      '국회의사당이 위치한 정치의 중심지이며, 금융 및 상업 지구입니다.',
+    category: '행정구역',
+    area: 24.55,
+    center: [126.908427, 37.526369],
+    location: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [126.88, 37.51],
+          [126.94, 37.51],
+          [126.94, 37.54],
+          [126.88, 37.54],
+          [126.88, 37.51],
+        ],
+      ],
     },
   },
 ];
--- src/mocks/map/dto.ts
+++ src/mocks/map/dto.ts
@@ -1,4 +1,11 @@
-import type { GeoJson, PointInfo } from '@/mocks/map/type';
+import type {
+  Feature,
+  GeoJson,
+  Geometry,
+  PointInfo,
+  Polygon,
+  PolygonInfo,
+} from '@/mocks/map/type';
 import type { ResponseDTO } from '@/mocks/shared/type';
 
 import { HttpResponse } from 'msw';
@@ -6,12 +13,31 @@ import { HttpResponse } from 'msw';
 /**
  * point 요청 데이터 dto
  */
-type PointListResponse = GeoJson<PointInfo>[];
+type PointListResponse = Geometry<PointInfo>[];
 
-export function createPointListResponse(data: GeoJson<PointInfo>[]): Response {
+export function createPointListResponse(data: Geometry<PointInfo>[]): Response {
   const response: ResponseDTO<PointListResponse> = {
     success: true,
     data: data,
+  };
+  return HttpResponse.json(response);
+}
+
+/**
+ * polygon 요청 데이터 dto
+ */
+
+type PolygonListResponse = GeoJson<Polygon, PolygonInfo>;
+
+export function createPolygonListResponse(
+  data: Feature<Polygon, PolygonInfo>[]
+): Response {
+  const response: ResponseDTO<PolygonListResponse> = {
+    success: true,
+    data: {
+      type: 'FeatureCollection',
+      features: data,
+    },
   };
   return HttpResponse.json(response);
 }
