b78981a8893648f2e887e21c1872e41c0f3607ec
Kimhyunjin4455 <dp4455@naver.com>
2025-08-24 19:27:16 +0900

    refactor: 로그인/회원가입 페이지 리팩토링1(pages/ui/index.tsx 통합)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,7 @@
-import { LoginForm } from '@/features/Login/ui';
+// import { LoginForm } from '@/features/Login/ui';
 
-export default function LoginPage() {
-  return <LoginForm />;
-}
+// export default function LoginPage() {
+//   return <LoginForm />;
+// }
+export { default } from './ui';
+
---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,6 @@
+// import { SignUpForm } from '@/features/SignUp/ui';
+
+// export default function SignUpPage() {
+//   return <SignUpForm />;
+// }
+export { default } from './ui';
